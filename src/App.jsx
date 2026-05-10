3a597eec4b65c41911fa3cff5444b3da60a1623c
nulacas-patepluma <nurialaracastillo@gmail.com>
2026-05-10 00:22:07 +0000

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import WelcomeHeader from './WelcomeHeader';
+import FactGenerator from './Fact Generator';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <WelcomeHeader />
+      <FactGenerator />
     </div>
   );
 }
